45eb710052b3fee6bad1040b661ed8d30189ae10
Camysgama <camysgama@outlook.com>
2024-09-04 20:38:21 -0300

    fix: remove vercel

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,25 @@ import { Controller } from './api/controllers/Controller';
 import { schemas } from './schemas/StorageSchema';
 import fastifyCors from '@fastify/cors';
 
+const app = fastify()
 
-export function BuildServer() {
-    const server = fastify()
+app.register(require('@fastify/formbody'))
 
-    server.register(require('@fastify/formbody'))
-
-    for (const schema of schemas) {
-        server.addSchema(schema)
-    }
-
-    //Registrar a controller no service, para ter acesso ao server na controller
-    server.register(Controller)
-
-    server.register(fastifyCors, {
-        origin: '*',
-        methods: ['GET', 'POST']
-    })
-
-    return server;
+for (const schema of schemas) {
+    app.addSchema(schema)
 }
+
+//Registrar a controller no service, para ter acesso ao app na controller
+app.register(Controller)
+
+app.register(fastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST']
+})
+
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => {
+    console.log("Server running on port " + (process.env.PORT ?? "3333"))
+})
